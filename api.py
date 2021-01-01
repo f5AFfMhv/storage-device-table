4d5bb7d8009b64362dba60122dde42450dc9e519
f5AFfMhv <f5AFfMhv@protonmail.com>
2021-01-01 19:25:55 +0200

    cleanup

--- api.py
+++ api.py
@@ -11,48 +11,64 @@ from json2html import *
 import sqlite3
 import requests
 
+# Variables
+BASE = '/api/v1/resources/servers' # API uri
+BASE_URL = 'http://localhost:5000' + BASE
+DB_FILE = 'servers.db'
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
+app.config["DEBUG"] = True # Enable stdout logging
 
+# Method to convert data from SQLite DB to dictionary (from "API with flask" tutorial)
 def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
     return d
 
-def page_not_found(e):
-    return make_response(jsonify({'error': 'Not found'}), e)
-
-def bad_request(e):
-    return make_response(jsonify({'error': 'Bad request'}), e)
-
-@app.route('/', methods=['GET'])
-def home():
-    #return "<h1>Server disk usage table</h1>"
-    r = requests.get('http://localhost:5000/api/v1/resources/servers/all')
-    return json2html.convert(json = r.json())
-
-@app.route('/api/v1/resources/servers/all', methods=['GET'])
-def api_all():
-    conn = sqlite3.connect('servers.db')
+# Method to read and parse data from SQLite DB
+def db_read(db, sql):
+    conn = sqlite3.connect(db)
     conn.row_factory = dict_factory
     cur = conn.cursor()
-    all_servers = cur.execute('SELECT * FROM servers;').fetchall()
-    return jsonify(all_servers)
+    return cur.execute(sql).fetchall()
+
+# Error handling methods
+def page_not_found(e):
+    return make_response(jsonify({'error':'Not found', 'status':e}), e)
+
+def bad_request(e):
+    return make_response(jsonify({'error':'Bad request', 'status':e}), e)
 
 @app.errorhandler(404)
 def not_found(e):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return make_response(jsonify({'error':'Not found', 'status':404}), 404)
 
-@app.route('/api/v1/resources/servers', methods=['GET'])
-def api_filter():
-    query_parameters = request.args
+# Server root page shows table with all records from DB
+@app.route('/', methods=['GET'])
+def home():
+    # Get data from API
+    r = requests.get(BASE_URL + '/all')
+    # Result --> JSON --> HTML
+    return json2html.convert(json = r.json())
 
-    id = query_parameters.get('id')
-    state = query_parameters.get('state')
-    name = query_parameters.get('name')
+# API OBTAIN INFORMATION
+# Get all records from DB in JSON
+@app.route(BASE + '/all', methods=['GET'])
+def get_all_records():
+    sql = ''' SELECT * FROM servers '''
+    all_servers = db_read(DB_FILE, sql)
+    return jsonify(all_servers)
 
+# Get specific records from DB
+@app.route(BASE, methods=['GET'])
+def get_record():
+    # Filter API requests by record ID, name or state
+    id = request.args.get('id')
+    state = request.args.get('state')
+    name = request.args.get('name')
+
+    # Build SQL query from given requests
     sql = "SELECT * FROM servers WHERE"
     to_filter = []
 
@@ -80,7 +96,7 @@ def api_filter():
     else:
         return jsonify(results)
 
-@app.route('/api/v1/resources/servers', methods=['POST'])
+@app.route(BASE, methods=['POST'])
 def create_record():
 
     if not request.json or not 'name' in request.json:
@@ -106,11 +122,11 @@ def create_record():
     conn.commit()
     return jsonify({'server': server}), 201
 
-@app.route('/api/v1/resources/servers', methods=['DELETE'])
+@app.route(BASE, methods=['DELETE'])
 def delete_record():
     query_parameters = request.args
     id = query_parameters.get('id')
-    r = requests.get('http://localhost:5000/api/v1/resources/servers?id=' + id)
+    r = requests.get(BASE_URL + '?id=' + id)
     if (r.status_code == 404):
         return bad_request(400)
     
@@ -121,12 +137,12 @@ def delete_record():
     conn.commit()
     return jsonify({'result': True}), 200
 
-@app.route('/api/v1/resources/servers', methods=['PUT'])
+@app.route(BASE, methods=['PUT'])
 def update_record():
     if not request.json or not 'id' in request.json:
         return bad_request(400)
     id = request.json.get('id')
-    r = requests.get('http://localhost:5000/api/v1/resources/servers?id=' + str(id))
+    r = requests.get(BASE_URL + '?id=' + str(id))
     if (r.status_code == 404):
         return bad_request(400)
     if 'state' in request.json and type(request.json['state']) != str:
@@ -150,7 +166,7 @@ def update_record():
     cur = conn.cursor()
     cur.execute(sql, server_list)
     conn.commit()
-    r = requests.get('http://localhost:5000/api/v1/resources/servers?id=' + str(id))
+    r = requests.get(BASE_URL + '?id=' + str(id))
     return jsonify({'server': r.json()})
     
 app.run(host='0.0.0.0')
